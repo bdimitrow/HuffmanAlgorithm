3f8ec6b2d029b7035cb929f774d7a9bdca56d63d
bdimitrow <bozhidar.dimitrow@gmail.com>
2020-12-02 17:35:25 +0200

    test

---
+++ tree.cpp
@@ -0,0 +1,5 @@
+//
+// Created by bozhidar on 12/2/20.
+//
+
+#include "tree.h"
---
+++ tree.h
@@ -0,0 +1,14 @@
+//
+// Created by bozhidar on 12/2/20.
+//
+
+#ifndef HUFFMANALGORITHM_TREE_H
+#define HUFFMANALGORITHM_TREE_H
+
+
+class tree {
+
+};
+
+
+#endif //HUFFMANALGORITHM_TREE_H
